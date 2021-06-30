6f72524c2978f4a0a0c6fcbedc9da649f6640a08
keychi25 <e165412y@st.u-gakugei.ac.jp>
2021-06-30 11:33:59 +0900

    finish: 25

--- 25_args.py
+++ 25_args.py
@@ -50,3 +50,54 @@ except ZeroDivisionError:
 assert safe_division_c(number=2, divisor=5) == 0.4
 assert safe_division_c(divisor=5, number=2) == 0.4
 assert safe_division_c(2, divisor=5) == 0.4
+
+
+def safe_division_d(numerator, denominator, /, *,
+    ignore_overflow=False, ignore_zero_division=False):
+    """位置専用引数として設定
+    """
+    try:
+        return numerator / denominator
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+assert safe_division_d(2, 5) == 0.4
+# safe_division_d(numerator=2, denominator=5) # TypeError: safe_division_d() got some positional-only arguments passed as keyword arguments: 'numerator, denominator'
+
+
+def safe_division_e(numerator, denominator, /,
+    ndigits=10, *,
+    ignore_overflow=False, ignore_zero_division=False):
+    """位置専用引数として設定
+    """
+    try:
+        fraction = numerator / denominator
+        return round(fraction, ndigits)
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+result = safe_division_e(22, 7)
+print(result)
+
+result = safe_division_e(22, 7, 5)
+print(result)
+
+
+result = safe_division_e(22, 7, ndigits=2)
+print(result)
